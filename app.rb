5216a846364d256f171c738fcd17759be51345e4
Ariel Feingold <feingold.ariel@gmail.com>
2018-03-29 21:05:17 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -33,7 +33,7 @@ class App < Sinatra::Base
     @operation = params[:operation]
     @number1 = params[:number1]
     @number2 = params[:number2]
-    " #{@number1 @operation @number2}.to_s"
+    " #{(@number1 @operation @number2).to_s}"
   end
 
 
